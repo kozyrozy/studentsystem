56aa9ba65045f4af1ffb43221b2c179f1ac484fa
Rosa Sulimanova <yourpersonalunicorn@gmail.com>
2023-11-19 12:36:38 +0300

    Initial commit: fully working back-end and DB

---
+++ studentsystem/src/main/java/com/mygroup/studentsystem/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.mygroup.studentsystem.controller;
+
+import com.mygroup.studentsystem.model.Student;
+import com.mygroup.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+}
