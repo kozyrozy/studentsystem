ae8360903e4963a0415a9cd0d28ddacd72c05ae9
Rosa Sulimanova <yourpersonalunicorn@gmail.com>
2023-11-28 20:09:53 +0300

    Implimented GET and DELETE methods on frontend

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container, Paper, Button } from '@mui/material';
@@ -9,6 +9,7 @@ export default function Student() {
     const paperStyle={padding:'50px 20px', width:600, margin:"20px auto"}
     const[name, setName]=useState('')
     const[address, setAddress]=useState('')
+    const[student, setStudents]=useState([])
 
     const handleClick=(e)=>{
       e.preventDefault()
@@ -20,6 +21,15 @@ export default function Student() {
         body:JSON.stringify(student)}
       ).then(()=>{console.log("New student added")}) 
     }
+
+    useEffect(()=>{
+      fetch("http://localhost:8080/student/getAll")
+      .then(res=>res.json())
+      .then((result)=>{
+          setStudents(result);
+        }
+      )
+    },[])
 
   return (
     <Container>
@@ -44,7 +54,27 @@ export default function Student() {
         variant="contained"
         onClick={handleClick}>
       Submit</Button>
+      <Button
+        color="error"
+        disabled={false}
+        size="large"
+        variant="contained">
+      Delete</Button>
     </Box>
+    </Paper>
+
+    <h1>STUDENTS</h1>
+    <Paper elevation={3} style={paperStyle}>
+
+      {student.map(student=>(
+        <Paper elevation={6} style={{margin:"10px", padding:"15px",textAlign:"left"}} key={student.id}>
+          Id: {student.id}<br/>
+          Name: {student.name}<br/>
+          Address: {student.address}
+        </Paper>
+          ))
+      }
+
     </Paper>
     </Container>
   );
